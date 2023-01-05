e165077554fbb17a7f215c28c99466ff0e3888da
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-01-05 18:29:06 +0900

    プロフィール画像のアップロード機能を追加。プロフィールページのビュー、ユーザモデルの修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:image, :name, :introduction)
+    params.require(:user).permit(:image, :name, :about)
   end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+
+  has_many :rooms
+
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: {maximum: 50}
+  validates :about, length: { maximum: 200 } 
 end
